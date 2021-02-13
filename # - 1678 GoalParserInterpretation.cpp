a5cddab571d85f2c5fd04b11da5610e84fa3d9c8
Sourav Mhatre <souravmhatre17@gmail.com>
2021-02-13 14:28:01 +0530

    Add files via upload

---
+++ # - 1678 GoalParserInterpretation.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    string interpret(string command) {
+        string result="";
+        for(int i =0;i<command.length();i++)
+        {
+
+            if(command.at(i) == 'G')
+            {
+                result += "G";
+            }
+            if(command.at(i) == '(')
+            {
+                if(command.at(i+1) == ')')
+                    result += "o";
+                if(command.at(i+1) == 'a')
+                    result += "al";
+            }
+        }
+        return result;
+
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    string C = "G()(al)";
+    s.interpret(C);
+    return 0;
+}
