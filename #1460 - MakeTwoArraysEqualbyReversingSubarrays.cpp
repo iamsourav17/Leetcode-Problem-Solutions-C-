3b1b00b1949e411aa200c10cc0f985a79d9e34d2
Sourav Mhatre <souravmhatre17@gmail.com>
2021-02-16 10:40:34 +0530

    Add files via upload

---
+++ #1460 - MakeTwoArraysEqualbyReversingSubarrays.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<array>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+
+        return target==arr;
+    }
+};
