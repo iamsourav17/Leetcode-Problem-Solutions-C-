9a83aa107f45d5db09a7a8eca6bbdc25349ae6a5
Sourav Mhatre <souravmhatre17@gmail.com>
2021-02-18 13:29:48 +0530

    Add files via upload

---
+++ #145 - BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    void postorder(TreeNode *root,vector<int> &v)
+    {
+        if(root == NULL) return;
+
+        postorder(root->left , v);
+        postorder(root->right , v);
+        v.push_back(root->val);
+
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root,ans);
+        return ans;
+
+    }
+};
