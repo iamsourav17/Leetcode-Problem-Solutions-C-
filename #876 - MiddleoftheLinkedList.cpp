2eeb309d30d019d3fcbe0b5aaa9a1ddda5f61052
Sourav Mhatre <souravmhatre17@gmail.com>
2021-03-17 22:35:10 +0530

    Add files via upload

---
+++ #876 - MiddleoftheLinkedList.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if(fast != NULL)
+        {
+            return slow ->next;
+        }
+
+        return slow;
+
+
+
+    }
+};
