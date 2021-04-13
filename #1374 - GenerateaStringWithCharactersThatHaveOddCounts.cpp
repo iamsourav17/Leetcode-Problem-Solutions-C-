6a3db1d7074609a7eb0cca3ab44af606a251c8c9
Sourav Mhatre <souravmhatre17@gmail.com>
2021-04-13 14:03:01 +0530

    Add files via upload

---
+++ #1374 - GenerateaStringWithCharactersThatHaveOddCounts.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+         string ans ="";
+
+        if(n % 2 != 0)
+        {
+            for(int i =0;i<n;i++)
+            {
+            ans.push_back('a');
+
+            }
+        }
+        else
+        {
+            for(int i =0;i<n-1;i++)
+            {
+            ans.push_back('a');
+
+            }
+            ans.push_back('b');
+        }
+        return ans;
+
+    }
+};
