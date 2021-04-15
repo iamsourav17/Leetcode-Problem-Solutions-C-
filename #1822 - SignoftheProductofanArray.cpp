8167ee6a9c2f84047b47010128d958a687f0ee52
Sourav Mhatre <souravmhatre17@gmail.com>
2021-04-15 12:47:12 +0530

    Add files via upload

---
+++ #1822 - SignoftheProductofanArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int product =1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] ==0) product =0;
+            if(nums[i] < 0) product *=-1;
+        }
+      return product;
+    }
+
+};
