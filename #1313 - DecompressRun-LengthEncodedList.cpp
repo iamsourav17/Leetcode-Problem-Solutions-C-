9457ce2f329836bd558d7186a1a176dd931b3243
Sourav Mhatre <souravmhatre17@gmail.com>
2021-06-10 15:51:00 +0530

    Add files via upload

---
+++ #1313 - DecompressRun-LengthEncodedList.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+         vector<int> answer;
+        for(int i = 0 ; i<nums.size(); i+=2)
+        {
+            answer.insert(answer.end(),nums[i],nums[i+1]);
+        }
+
+        return answer;
+
+    }
+};
